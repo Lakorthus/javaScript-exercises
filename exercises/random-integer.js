57f9d6f62666931aa0d3a6aca18a96c23ea09e3b
Lakorthus <julio28ve@gmail.com>
2022-06-05 22:32:53 +0100

    comented

--- exercises/random-integer.js
+++ exercises/random-integer.js
@@ -12,13 +12,14 @@ if (num == guestNumber) {
 }
 
 // ES6
-var num = Math.ceil(Math.random() * 10);
-console.log(num);
 
-var guestNumber = prompt("Guess the number between 1 and 10 inclusive");
+// var num = Math.ceil(Math.random() * 10);
+// console.log(num);
 
-if (num == guestNumber) {
-  console.log("Matched");
-} else {
-  console.log("Not matched, the number was " + guestNumber);
-}
+// var guestNumber = prompt("Guess the number between 1 and 10 inclusive");
+
+// if (num == guestNumber) {
+//   console.log("Matched");
+// } else {
+//   console.log("Not matched, the number was " + guestNumber);
+// }
