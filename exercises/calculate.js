c5ff606ea01cce26cf86bdd9a3c4d53e4bbe6032
Lakorthus <julio28ve@gmail.com>
2022-06-05 22:54:20 +0100

    calculate

---
+++ exercises/calculate.js
@@ -0,0 +1,25 @@
+// javaScript
+
+function multiplyBy() {
+  num1 = document.getElementById("firstNumber").value;
+  num2 = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() {
+  num1 = document.getElementById("firstNumber").value;
+  num2 = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML = num1 / num2;
+}
+
+function sumBy() {
+  num1 = document.getElementById("firstNumber").value;
+  num2 = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML = num1 + num2;
+}
+
+function minusBy() {
+  num1 = document.getElementById("firstNumber").value;
+  num2 = document.getElementById("secondNumber").value;
+  document.getElementById("result").innerHTML = num1 - num2;
+}
