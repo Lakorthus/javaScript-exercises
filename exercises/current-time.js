b8322bae0d3e5262a4eaa9c2f0fcd05178393ea0
Lakorthus <julio28ve@gmail.com>
2022-06-05 16:44:59 +0100

    print  current page

--- exercises/current-time.js
+++ exercises/current-time.js
@@ -2,7 +2,7 @@ var today = new Date();
 
 var day = today.getDay();
 var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-console.log("Today is : " + daylist[day] + ".");
+console.log("Today is : " + dayList[day] + ".");
 
 
 var hour = today.getHours();
